0e49e8fdacd4b64f963738e003011901cfdb4a9d
gomeseric <gomesericson.a@gmail.com>
2024-07-25 00:38:28 +0000

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <div>Hello MDC 27</div>;
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Home() {
+  const supabase = createClient();
+  const user = await supabase.auth.getUser();
+
+  return <div>Hello MDC 27 - {user?.data?.user?.email} </div>;
 }
